a0d5ba3c18b352a66d5b1a0a97a8235a5e05749f
Isuru Bandaranayake <isbana.git@gmail.com>
2019-06-02 12:02:54 +0530

    Fixed Indentation

--- LinkedList/Circular-Singly/Node.java
+++ LinkedList/Circular-Singly/Node.java
@@ -1,25 +1,24 @@
-public class Node {
-    private Node next;
-    private int value;
+public class Node{
+	private int value;
+	private Node next;
 
-    public Node(int value) {
-        this.next = null;
-        this.value = value;
-    }
+	public Node(int value){
+		this.value = value;
+	}
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
+	public int getValue(){
+		return value;
+	}
 
-    public void setData(int value) {
-        this.value = value;
-    }
-    
-    public Node getNext() {
-        return next;
-    }
+	public Node getNext(){
+		return next;
+	}
 
-    public int getValue() {
-        return value;
-    }
+	public void setValue(int value){
+		this.value = value;
+	}
+
+	public void setNext(Node next){
+		this.next = next;
+	}
 }
